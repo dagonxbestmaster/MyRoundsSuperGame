3609cee0d049fa86c385ff1b88f05c2d32a73e08
dagonxbestmaster <152633194+dagonxbestmaster@users.noreply.github.com>
2024-05-10 19:14:32 +0300

    Initial commit

---
+++ Assets/Scripts/Bonuses/HitTypeBonusApplier.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// Добавили наследование от BonusApplier
+public class HitTypeBonusApplier : MonoBehaviour, BonusApplier
+{
+  [SerializeField] private GameObject _defaultHitPrefab  ; // Префаб эффекта обычного попадания
+  [SerializeField] private GameObject _explosionHitPrefab; // Префаб эффекта попадания со взрывом
+  public void ApplyBonus(List<BonusType> existingBonusTypes, GameObject root) // Применяем бонусы к пуле
+  {
+        // Получаем объект типа попадания
+        // С учётом собранных игроком бонусов
+        int finalHitType = GetFinalHitType(existingBonusTypes);
+        Apply(finalHitType, root);  // Применяем объект типа попадания к исходному объекту
+  }
+  private BulletHit GetHit(List<BonusType> existingBonusTypes) // Получаем объект типа попадания
+    {
+        int finalHitType = GetFinalHitType(existingBonusTypes);
+        return CreateBulletHit(finalHitType); // Возвращаем объект типа попадания
+    }
+
+    private BulletHit CreateBulletHit(int finalHitType)
+    {
+        BulletHit hit = null; // Создаём объект типа попадания
+        if (finalHitType == 0)
+        {                                           // Если взрывной бонус отсутствует
+            hit = new DefaultBulletHit(finalHitType, _defaultHitPrefab);     // Создаём обычное попадание
+        }
+        else
+        {                                                           // Иначе
+            hit = new ExplosionBulletHit(finalHitType, _explosionHitPrefab); // Создаём попадание со взрывом
+        }
+
+        return hit;
+    }
+
+    private static int GetFinalHitType(List<BonusType> existingBonusTypes)
+    {
+        int finalHitType = 0;                                // Задаём начальную силу взрыва
+        for (int i = 0; i < existingBonusTypes.Count; i++)
+        { // Проходим по собранным игроком бонусам
+            int hitType = 0;                                   // Задаём переменную для типа попадания
+            switch (existingBonusTypes[i])                     // Выбираем действие в зависимости от типа бонуса
+            {
+                case BonusType.SmallExplosionHit: hitType = 1; break; // Если бонус — слабый взрыв
+                case BonusType.MediumExplosionHit: hitType = 2; break; // Если бонус — средний взрыв
+                case BonusType.LargeExplosionHit: hitType = 3; break; // Если бонус — сильный взрыв
+            }
+
+            if (finalHitType < hitType)
+            { // Если начальная силы взрыва < текущей
+                finalHitType = hitType; // Приравниваем начальное значение к текущему
+            }
+        }
+
+        return finalHitType;
+    }
+
+    private void Apply(int finalHitType, GameObject root) // Применяем объект типа попадания к исходному объекту
+  {
+        // Ищем все компоненты в дочерних объектах root
+        // Которые зависят от типа попадания
+        BulletHit hit = CreateBulletHit(finalHitType);
+        IHitTypeBonusDependent[] dependents = root.GetComponentsInChildren<IHitTypeBonusDependent>();
+    for (int i = 0; i < dependents.Length; i++) { // Проходим по найденным компонентам
+      dependents[i].SetHit(hit);                  // Вызываем для каждого метод SetHit()
+    }
+  }
+}
